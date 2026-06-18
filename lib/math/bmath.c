1e253eae5b5027a8bd98e5fa1a254bad009005dd
Admin <dc2000a@gmail.com>
2026-06-18 01:06:44 -0700

    updated main

--- lib/math/bmath.c
+++ lib/math/bmath.c
@@ -1,11 +1,10 @@
 #include <stdbool.h>
 #include "bmath.h"
-#include <math.h>
+
 float b_lerp(float a, float b, float step){
     return (1-step) * a + step * b;
 }
 
-float floor(float input){
 
-}
+
 
--- src/birdi.c
+++ src/birdi.c
@@ -10,18 +10,33 @@
 #include "raylib.h"
 
 
-struct userOptions{
+typedef struct userOptions{
     int screenWidth;
     int screenHeight;
     int targetFPS;
-    char windowTitle[128];
-};
+    char* windowTitle;
+}userOptions;
+
+typedef enum GameScreen{
+    MENU = 0,
+    SETUP,
+    GAME
+}GameScreen;
 
 int main(void) {
+    userOptions defaultOptions = {800,600,60,"Window"};
+    
+
+    InitWindow(defaultOptions.screenWidth, defaultOptions.screenHeight, defaultOptions.windowTitle);
+    
+    SetTargetFPS(defaultOptions.targetFPS);
+    
     while (!WindowShouldClose()) {
 
+        BeginDrawing();
+            ClearBackground(GRAY);
+        EndDrawing();
     }
-
     CloseWindow();
     return 0;
 }
