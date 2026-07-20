b6a066b2ac1589d4d431fb444c212d6baaa8b3ec
Admin <dc2000a@gmail.com>
2026-07-20 16:02:34 -0700

    updating project structure, setting up some basic stuff

--- lib/b_core.h
+++ lib/b_core.h
@@ -1,23 +1,12 @@
 #pragma once
 
-
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct{
-    char* data;
-    size_t length;
-}b_String_slice;
 
-typedef struct{
-    void* alloc;
-    size_t offset;
-}b_alloc;
-
-
+//simple math defs
 double b_lerp(double a, double b, double step);
-
 
 // basic RNG
 uint64_t INIT_SEED(void);
@@ -28,10 +17,17 @@ float normalize64(uint64_t seed);
 uint64_t splitmix64(uint64_t seed);
 uint64_t xorshift64(uint64_t seed);
 
-b_String_slice* conversion_fromCstring(char* in_str);
-char* conversion_toCstring(b_String_slice in_str);
-b_String_slice slice(b_String_slice in_str, size_t startpos, size_t endpos);
-b_String_slice string_replace_at(b_String_slice in_str, char replace, size_t index, size_t desiredIndex);
-b_String_slice fixBufOverflow(char* in_str);
+
+//string stuff
+typedef struct{
+    char* data;
+    size_t length;
+}b_String;
+
+b_String* conversion_fromCstring(char* in_str);
+char* conversion_toCstring(b_String in_str);
+b_String slice(b_String in_str, size_t startpos, size_t endpos);
+b_String string_replace_at(b_String in_str, char replace, size_t index, size_t desiredIndex);
+b_String fixBufOverflow(char* in_str);
 
 
--- lib/b_string.c
+++ lib/b_string.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-b_String_slice* conversion_fromCstring(char* in_str){
+b_String* conversion_fromCstring(char* in_str){
     size_t len = strlen(in_str);
-    b_String_slice tmp = {NULL, len};
-    b_String_slice* converted = malloc(sizeof(tmp));
+    b_String tmp = {NULL, len};
+    b_String* converted = malloc(sizeof(tmp));
     for (size_t i = 0; i<len; i++) {
         converted->data[i] = in_str[i];
     }
     return converted;
 }
 
-// char* conversion_toCstring(b_String_slice in_str){
+// char* conversion_toCstring(b_String in_str){
 //
 // }
 //
-// b_String_slice slice(b_String_slice in_str, size_t startpos, size_t endpos){
+// b_String slice(b_String in_str, size_t startpos, size_t endpos){
 //
 // }
 //
-// b_String_slice string_replace_at(b_String_slice in_str, char replace, size_t index, size_t desiredIndex){
+// b_String string_replace_at(b_String in_str, char replace, size_t index, size_t desiredIndex){
 //
 // }
 //
-// b_String_slice fixBufOverflow(char* in_str);
+// b_String fixBufOverflow(char* in_str);
 //
